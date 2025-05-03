3d8ad53b9f413e0b4c780b109b49a942c547f9a9
Jianqing <2024023604@m.scnu.edu.cn>
2025-05-03 16:15:05 +0800

    25/05/03

--- Numerical_stability_test.py
+++ Numerical_stability_test.py
@@ -1,61 +1,25 @@
 import MeepMetalens as mm
-import meep as mp
 import numpy as np
-import sqlite3
-import os
+from scipy.io import savemat
+# 基准数据
+parameter=[0.4, 0.6, 0.3, 0.11, 0.12]
+wav=0.800
 
-def main(parameter,index):
-    p, h, l, w, r=parameter[0], parameter[1], parameter[2], parameter[3], parameter[4]
-    nsio2 = 1.4608
-    ntio2 = 2.35
-    sio2=mm.medium_from_nk(nsio2,0,1/0.532)
-    tio2=mm.medium_from_nk(ntio2,0,1/0.532)
-    
-    if index==1:  
-        geometry=mm.fishnetset(sio2, tio2, p, h, l, w, r)
-    elif index==0:
-        geometry=[]
+fish=mm.Fishnet(parameter=parameter,wavelength=wav)
+phase1, flux_val1=fish.benchmarkComputation()
 
-    dpml = 1
-    pml_layers = [mp.PML(dpml, direction=mp.Z)]
+L=np.linspace(0.1,0.15,10)
+phase532=np.zeros_like(L)
+trans532=np.zeros_like(L)
 
-    wavelength = 0.532
-    frequency = 1 / wavelength
-    source_z = -0.1
+for i,l in enumerate(L):
+    parameter[4]=l
+    fish=mm.Fishnet(parameter=parameter,wavelength=wav)
+    phase2, flux_val2=fish.Computation()
+    phase532[i]=phase2-phase1
+    trans532[i]=flux_val2[0]/flux_val1[0]
 
-    sources = [mp.Source(mp.ContinuousSource(frequency=frequency),
-                        component=mp.Ex,
-                        center=mp.Vector3(z=source_z),
-                        amplitude=1,
-                        size=mp.Vector3(p, p)
-                        )
-               ]
-    sim = mp.Simulation(cell_size=mp.Vector3(p,p,4),
-                        geometry=geometry,
-                        sources=sources,
-                        resolution=50,
-                        boundary_layers=pml_layers,
-                        default_material=mp.Medium(epsilon=1.0),
-                        dimensions=3,
-                        symmetries=[],
-                        k_point=mp.Vector3()  # 周期性方向设置为 Gamma 点
-                        )
+data = {'phase800_py': phase532, 'trans800_py': trans532}
 
-    point_monitor, plane_monitor=mm.classicMonitorGroup(sim,p,0.8,wavelength)
-    sim.run(until=100)
-
-    data = sim.get_dft_array(point_monitor, num_freq=0, component=mp.Ex)
-    phase = np.angle(data)
-    flux_val = mp.get_fluxes(plane_monitor)
-
-    return phase, flux_val
-def findDataBase():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.dirname(current_dir)
-    data_dir = os.path.join(parent_dir, "data")
-    db_path = os.path.join(data_dir, "structures.db")
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    return cursor
-
-cursor=findDataBase()
+# 保存为 .mat 文件
+savemat('/mnt/d/WORK/Achromatic_metalens_design_in_Windows/Numerical_stability_test/py_r_800.mat', data)
